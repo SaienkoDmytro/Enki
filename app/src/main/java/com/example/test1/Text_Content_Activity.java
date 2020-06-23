aa5427c0be4c29dc1461b51ee11d61d9373de691
SaienkoDmytro <saenko.d.s@gmail.com>
2020-06-23 21:09:35 +0300

    before list changer set

---
+++ app/src/main/java/com/example/test1/Text_Content_Activity.java
@@ -0,0 +1,15 @@
+package com.example.test1;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Text_Content_Activity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text_content);
+    }
+}
